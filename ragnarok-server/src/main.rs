16a4d97ce9baf8dc9472156f729dc3cc513b57a5
belagarion111 <alessandro-busso@libero.it>
2023-04-22 22:54:13 +0200

    add jwt generation

--- ragnarok-server/src/main.rs
+++ ragnarok-server/src/main.rs
@@ -1,42 +1,41 @@
-use actix_web::{post, web, App ,  HttpServer, HttpResponse};
+use actix_web::{post, web, App, HttpResponse, HttpServer};
 use serde::Deserialize;
 use serde::Serialize;
 
 use hmac::{Hmac, Mac};
-use jwt::VerifyWithKey;
+use jwt::SignWithKey;
 use sha2::Sha256;
 use std::collections::BTreeMap;
-
-
+use std::string::String;
 
 // I Post this object from Angular Ragnarok
-#[derive(Deserialize)]
-#[derive(Serialize)]
+#[derive(Deserialize, Serialize)]
 struct AppState {
     username: String,
     password: String,
-    jwt     : Option<String>
+    jwt: Option<String>,
 }
 
-fn jwtGenerator(username : String ) -> String{
-    let key: Hmac<Sha256> = Hmac::new_from_slice(b"mySecret")?;
+fn jwtGenerator(username: String) -> String {
+    let key: Hmac<Sha256> = Hmac::new_from_slice(b"mySecret").unwrap();
     let mut claims = BTreeMap::new();
     claims.insert("username", username);
-    claims.sign_with_key(&key).unwrap();
+    let token_str = claims.sign_with_key(&key).unwrap();
+    format!("{}", token_str)
 }
 
 #[post("/login")]
-async fn login(data: web::Json<AppState>)  -> HttpResponse  {
-    println!("Welcome {}!" ,data.username );
+async fn login(mut data: web::Json<AppState>) -> HttpResponse {
+    println!("Welcome {}!", data.username);
+    let username = data.username.to_string();
+    data.jwt = Some(String::from(jwtGenerator(username)));
     HttpResponse::Ok().json(data)
 }
 
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(login)
-    }).bind(("127.0.0.1", 8083))?
-      .run().await
+    HttpServer::new(|| App::new().service(login))
+        .bind(("127.0.0.1", 8083))?
+        .run()
+        .await
 }
